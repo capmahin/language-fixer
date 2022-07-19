4cea04af7c01d75d986d8a0ce927e760e09cb3ec
NABIL <johayeriqbal@gmail.com>
2022-07-20 01:47:52 +0600

    added nav and footer

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/Shared/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/Shared/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div>
+      <NavBar></NavBar>
+      <Routes>
+        <Route></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
