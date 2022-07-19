0ffec2e1bcdea496cce5a37b69d0b9d238b2b572
capmahin <mahinulhuque1996@gmail.com>
2022-07-20 04:03:14 +0600

    add routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import NavBar from './components/Shared/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Footer from './components/Shared/Footer';
+import "./App.css";
+import NavBar from "./components/Shared/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Shared/Footer";
+import About from "./components/Pages/About/About";
+import Home from "./components/Pages/Home/Home";
+import Login from "./components/Pages/Login/Login";
+import Contact from "./components/Pages/Contact/Contact";
+import Reviews from "./components/Pages/Reviews/Reviews";
 
 function App() {
   return (
     <div>
       <NavBar></NavBar>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/reviews" element={<Reviews />} />
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -1,37 +1,65 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
-
-
-    const navItems = <>
-        <li className='my-1 lg:mr-2 cursor-pointer'>Home</li>
-        <li className='my-1 lg:mr-2 cursor-pointer'>About</li>
-        <li className='my-1 lg:mr-2 cursor-pointer'>My Profile</li>
-        <li className='my-1 lg:mr-2 cursor-pointer'>Contact Us</li>
+  const navItems = (
+    <>
+      <li className="my-1 lg:mr-2 cursor-pointer">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="my-1 lg:mr-2 cursor-pointer">
+        <Link to="about">About</Link>
+      </li>
+      <li className="my-1 lg:mr-2 cursor-pointer">
+        <Link to="login">Login</Link>
+      </li>
+      <li className="my-1 lg:mr-2 cursor-pointer">
+        <Link to="contact">Contact Us</Link>
+      </li>
+      <li className="my-1 lg:mr-2 cursor-pointer">
+        <Link to="reviews">Reviews</Link>
+      </li>
     </>
+  );
 
-    return (
-        <div>
-            <div className="navbar bg-base-100 lg:justify-evenly">
-                <div className="navbar-start">
-                    <div className="dropdown">
-                        <label tabindex="0" className="btn btn-ghost lg:hidden">
-                            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-                        </label>
-                        <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                            {navItems}
-                        </ul>
-                    </div>
-                    <a href='/' className="btn btn-ghost normal-case text-xl">Language Fixer</a>
-                </div>
-                <div className="navbar-center hidden lg:flex ">
-                    <ul className="menu menu-horizontal p-0">
-                        {navItems}
-                    </ul>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="navbar bg-base-100 lg:justify-evenly">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabindex="0" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navItems}
+            </ul>
+          </div>
+          <a href="/" className="btn btn-ghost normal-case text-xl">
+            Language Fixer
+          </a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex ">
+          <ul className="menu menu-horizontal p-0">{navItems}</ul>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
