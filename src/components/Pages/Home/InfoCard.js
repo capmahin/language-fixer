97aff181b409fbc30322167329bcd5ef4933383c
capmahin <mahinulhuque1996@gmail.com>
2022-07-20 06:12:52 +0600

    fix width problem

--- src/components/Pages/Home/InfoCard.js
+++ src/components/Pages/Home/InfoCard.js
@@ -4,7 +4,7 @@ const InfoCard = ({ img, cardTitle }) => {
   return (
     <div class="card lg:card-side bg-base-100 shadow-xl">
       <figure className="pl-5 pt-5">
-        <img className="w-48" src={img} alt="Album" />
+        <img className="w-36" src={img} alt="Album" />
       </figure>
       <div class="card-body">
         <h2 class="card-title">{cardTitle}</h2>
