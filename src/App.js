5bd4abb6406f9005cc6baec164743a2700c24b01
Shahria-Turag <shahriarturag.st@gmail.com>
2022-07-22 20:09:47 +0600

    quiz

--- src/App.js
+++ src/App.js
@@ -7,21 +7,23 @@ import Login from "./components/Pages/Login/Login";
 import Contact from "./components/Pages/Contact/Contact";
 import Reviews from "./components/Pages/Reviews/Reviews";
 import Learn from "./components/Pages/Learn/Learn";
+import Quizzes from "./components/Pages/Home/Quizzes";
 
 function App() {
-  return (
-    <div>
-      <NavBar></NavBar>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/learn" element={<Learn />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/reviews" element={<Reviews />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div>
+            <NavBar></NavBar>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/learn" element={<Learn />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/reviews" element={<Reviews />} />
+                <Route path="/quiz" element={<Quizzes />} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
