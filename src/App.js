d84ebcfe05422c60fb2652d7331b320daec0f24b
Shahria-Turag <shahriarturag.st@gmail.com>
2022-07-26 17:51:29 +0600

    trial lesson added

--- src/App.js
+++ src/App.js
@@ -10,10 +10,11 @@ import Learn from "./components/Pages/Learn/Learn";
 
 import Quizzes from "./components/Pages/Home/Quizzes";
 import Footer from "./components/Pages/Shared/Footer";
+import TrialLesson from "./components/Pages/Home/TrialLesson";
 
 function App() {
     return (
-        <div className="bg-primary">
+        <div className="bg-primary text-white">
             <NavBar></NavBar>
             <Routes>
                 <Route path="/" element={<Home />} />
@@ -23,9 +24,10 @@ function App() {
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/reviews" element={<Reviews />} />
                 <Route path="/quiz" element={<Quizzes />} />
+                <Route path="/trialLesson" element={<TrialLesson />} />
             </Routes>
             <Footer></Footer>
         </div>
     );
-};
+}
 export default App;
