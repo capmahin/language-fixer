d9fdbe1bc6844f498f5efc8a83669209f7a0156e
Shahria-Turag <shahriarturag.st@gmail.com>
2022-08-08 11:21:24 +0600

    add extra section

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -6,18 +6,20 @@ import AddReview from "./AddReview";
 import Services from "./Services";
 import WhyLF from "./WhyLF";
 import Reviews from "./Reviews";
+import Effective from "./Effective";
 
 const Home = () => {
-  return (
-    <div>
-      <Banner></Banner>
-      <Info></Info>
-      <Services></Services>
-      <WhyLF></WhyLF>
-      <Reviews></Reviews>
-      <AddReview></AddReview>
-    </div>
-  );
+    return (
+        <div>
+            <Banner></Banner>
+            <Info></Info>
+            <Services></Services>
+            <WhyLF></WhyLF>
+            <Effective />
+            <Reviews></Reviews>
+            <AddReview></AddReview>
+        </div>
+    );
 };
 
 export default Home;
