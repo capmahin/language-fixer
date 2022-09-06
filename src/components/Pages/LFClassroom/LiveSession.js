f059c2479ddee9f3d76122c923180dda5d7cc31e
Shahria-Turag <shahriarturag.st@gmail.com>
2022-09-06 15:32:46 +0600

    test

--- src/components/Pages/LFClassroom/LiveSession.js
+++ src/components/Pages/LFClassroom/LiveSession.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const LiveSession = () => {
     return (
-        <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center m-52">
+            <h1 className="text-white">Please join the live session</h1>
             <button className="btn bg-accent my-5 border-0 text-white rounded-full w-44 hover:bg-white hover:text-accent hover:border-accent hover:border-2 modal-button ">
                 <a
                     target="blank"
--- src/components/Pages/LFClassroom/ToDoList/ToDoList.js
+++ src/components/Pages/LFClassroom/ToDoList/ToDoList.js
@@ -5,48 +5,50 @@ import { useDispatch, useSelector } from "react-redux";
 import { deleteAll } from "./redux/todoapp/actions/index";
 
 function ToDoList() {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const todos = useSelector((state) => state.operationsReducer);
+    const todos = useSelector((state) => state.operationsReducer);
 
-  const [editFormVisibility, setEditFormVisibility] = useState(false);
+    const [editFormVisibility, setEditFormVisibility] = useState(false);
 
-  const [editTodo, setEditTodo] = useState("");
+    const [editTodo, setEditTodo] = useState("");
 
-  const handleEditClick = (todo) => {
-    setEditFormVisibility(true);
-    setEditTodo(todo);
-  };
+    const handleEditClick = (todo) => {
+        setEditFormVisibility(true);
+        setEditTodo(todo);
+    };
 
-  const cancelUpdate = () => {
-    setEditFormVisibility(false);
-  };
+    const cancelUpdate = () => {
+        setEditFormVisibility(false);
+    };
 
-  return (
-    <div class="hero-content text-center min-h-screen ">
-      <div class="max-w-md">
-        <br></br>
-        <h1 className="text-center text-4xl  text-yellow-400">TODO-List</h1>
-        <Form
-          editFormVisibility={editFormVisibility}
-          editTodo={editTodo}
-          cancelUpdate={cancelUpdate}
-        />
-        <Todos
-          handleEditClick={handleEditClick}
-          editFormVisibility={editFormVisibility}
-        />
-        {todos.length > 1 && (
-          <button
-            className="btn btn-info btn-md delete-all"
-            onClick={() => dispatch(deleteAll())}
-          >
-            DELETE ALL
-          </button>
-        )}
-      </div>
-    </div>
-  );
+    return (
+        <div class="hero-content text-center min-h-screen">
+            <div class="max-w-md">
+                <br></br>
+                <h1 className="text-center text-4xl  text-yellow-400">
+                    TODO-List
+                </h1>
+                <Form
+                    editFormVisibility={editFormVisibility}
+                    editTodo={editTodo}
+                    cancelUpdate={cancelUpdate}
+                />
+                <Todos
+                    handleEditClick={handleEditClick}
+                    editFormVisibility={editFormVisibility}
+                />
+                {todos.length > 1 && (
+                    <button
+                        className="btn btn-accent btn-md delete-all"
+                        onClick={() => dispatch(deleteAll())}
+                    >
+                        DELETE ALL
+                    </button>
+                )}
+            </div>
+        </div>
+    );
 }
 
 export default ToDoList;
